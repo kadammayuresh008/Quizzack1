c8c28cf5f72e84978c9c7b1e9add16835feb65fb
Mayu008 <kadam.ms@somaiya.edu>
2021-04-01 18:44:05 +0530

    chnages

--- Quiz_app/urls.py
+++ Quiz_app/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('profile/',user_views.profile, name="profile"),
     path('login/',auth_views.LoginView.as_view(template_name='user/login.html'),name="login"),
     path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name="logout"),
-    path('',include('blog.urls')),
+    # path('',include('blog.urls')),
+    path('',blog_views.home, name="home"),
 ]
 
 if settings.DEBUG:
